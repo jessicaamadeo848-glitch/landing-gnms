4a4ad806ac37d5b8da3ecb932b2c5a8b4e09775b
jessicaamadeo848-glitch <jessicaamadeo848@gmail.com>
2026-02-27 11:40:34 -0300

    Update events.js

--- api/events.js
+++ api/events.js
@@ -1,5 +1,4 @@
-export default async function handler(req, res) {
-
+module.exports = async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -73,4 +72,4 @@ export default async function handler(req, res) {
       error: error.message
     });
   }
-}
+};
